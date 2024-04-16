25b2913769755d4cf56d268dba2fcc052cfebac8
NikoEduard <nied88218@gmail.com>
2024-04-16 22:28:24 +0200

    way to data.json

--- src/script.js
+++ src/script.js
@@ -18,7 +18,7 @@ function renderMainBtns() { }
 
 async function dataFromJson() {
   try {
-    const response = await fetch("../data.json");
+    const response = await fetch("/data.json");
     const data = await response.json();
     return data;
   } catch (err) {
