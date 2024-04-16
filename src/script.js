1aabd32bfeb441b7bbcbb767176cb2793f076b8e
NikoEduard <nied88218@gmail.com>
2024-04-16 22:30:53 +0200

    way to data.json

--- src/script.js
+++ src/script.js
@@ -19,6 +19,9 @@ function renderMainBtns() { }
 async function dataFromJson() {
   try {
     const response = await fetch("../data.json");
+    if (!response.ok) { 
+      throw new Error('Network response was not ok: ' + response.statusText);
+    }
     const data = await response.json();
     return data;
   } catch (err) {
